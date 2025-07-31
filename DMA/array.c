fe3b245d048c3993d04182c0c1949226a0391317
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-07-31 23:02:35 +0530

    Update the code snippet to made functions to print persons

--- DMA/array.c
+++ DMA/array.c
@@ -6,6 +6,9 @@ typedef struct {
     int age;
 }Person;
 
+void printPerson(Person *arr, int n);
+void makePerson(Person *arr, int n);
+
 int main() {
     int n;
     printf("Enter the Size of the array: ");
@@ -13,27 +16,32 @@ int main() {
     printf("-----------------------------\n");
 
     Person *ptr = (Person *)malloc(n * sizeof(Person));
+    makePerson(ptr,n);
 
+    printf("--------------Result-------------\n");
+    printPerson(ptr, n);
+
+    free(ptr);
+    return 0;
+}
+
+void printPerson(Person *arr, int n) {
+    for(int i = 0; i < n; i++) {
+        printf("Person %d\n", i+1);
+        printf("Name: %s", arr[i].name);
+        printf("Age: %d\n", arr[i].age);
+    }
+}
+
+void makePerson(Person *arr, int n) {
     for(int i = 0; i < n; i++) {
         printf("Person %d\n", i+1);
         getchar();
         
         printf("Enter the name: ");
-        fgets(ptr[i].name, sizeof(ptr[i].name), stdin);
+        fgets(arr[i].name, sizeof(arr[i].name), stdin);
         
         printf("Enter the age: ");
-        scanf("%d", &ptr[i].age);
-        printf("-----------------------------\n");
+        scanf("%d", &arr[i].age);
     }
-
-    printf("--------------Result-------------\n");
-
-    
-    for(int i = 0; i < n; i++) {
-        printf("Enter the name: %s", ptr[i].name);
-        printf("Enter the age: %d\n", ptr[i].age);
-    }
-
-    free(ptr);
-    return 0;
 }
