62ece2e8c506692ae4d18467ea92b70f961c8d58
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-09-04 15:34:35 +0530

    Implement queue using array

--- Quoue/arrayQuoue.c
+++ Quoue/arrayQuoue.c
@@ -1,62 +1,67 @@
 #include <stdio.h>
 #define MAX_SIZE 5
 
-typedef struct {
-    int data[MAX_SIZE];
-    int front;
-    int rear;
-} Queue;
-
-void initQueue(Queue *q) {
-    q->front = 0;
-    q->rear = -1;
+int isFull(int front, int rear) {
+    // Queue is full if next position of rear is front (circular)
+    return ((rear + 1) % MAX_SIZE) == front;
 }
 
-int isFull(Queue *q) {
-    return q->rear == MAX_SIZE - 1;
+int isEmpty(int front, int rear) {
+    // Queue is empty if front == -1
+    return front == -1;
 }
 
-int isEmpty(Queue *q) {
-    return q->front > q->rear;
-}
-
-int enqueue(Queue *q, int value) {
-    if (isFull(q)) {
-        // Queue is full
+int enqueue(int q[], int value, int *front, int *rear) {
+    if (isFull(*front, *rear)) {
+        printf("Queue is full\n");
         return 0;
     }
-    q->rear++;
-    q->data[q->rear] = value;
+    if (isEmpty(*front, *rear)) {
+        *front = *rear = 0;
+    } else {
+        *rear = (*rear + 1) % MAX_SIZE;
+    }
+    q[*rear] = value;
     return 1;
 }
 
-int dequeue(Queue *q, int *value) {
-    if (isEmpty(q)) {
-        // Queue is empty
+int dequeue(int q[], int *value, int *front, int *rear) {
+    if (isEmpty(*front, *rear)) {
+        printf("Queue is empty\n");
         return 0;
     }
-    *value = q->data[q->front];
-    q->front++;
+    *value = q[*front];
+    if (*front == *rear) {
+        // Queue has only one element, reset to empty
+        *front = *rear = -1;
+    } else {
+        *front = (*front + 1) % MAX_SIZE;
+    }
     return 1;
 }
 
-void printQueue(Queue *q) {
-    if (isEmpty(q)) {
+void printQueue(int q[], int front, int rear) {
+    if (isEmpty(front, rear)) {
         printf("Queue is empty\n");
         return;
     }
     printf("Queue: ");
-    for (int i = q->front; i <= q->rear; i++) {
-        printf("%d ", q->data[i]);
+    int i = front;
+    while (1) {
+        printf("%d ", q[i]);
+        if (i == rear)
+            break;
+        i = (i + 1) % MAX_SIZE;
     }
     printf("\n");
 }
 
 int main() {
-    Queue q;
-    initQueue(&q);
+    int queue[MAX_SIZE];
+    int rear = -1, front = -1;
+    int choice, n, res;
+    
     while (1) {
-        int choice, n;
         printf("Enter the choice: \n");
         printf("1. Enqueue\n");
         printf("2. Dequeue\n");
@@ -68,13 +73,15 @@ int main() {
             case 1:
                 printf("Enter the value to enqueue: ");
                 scanf("%d", &n);
-                enqueue(&q, n);
+                enqueue(queue, n, &front, &rear);
                 break;
             case 2:
-                dequeue(&q, &n);
+                res = dequeue(queue, &n, &front, &rear);
+                if (res)
+                    printf("Dequeued: %d\n", n);
                 break;
             case 3:
-                printQueue(&q);
+                printQueue(queue, front, rear);
                 break;
             case 4:
                 return 0;
