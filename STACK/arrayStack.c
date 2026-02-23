2338717e023558583301300c62b19ee12a824b67
Ash310u <ashutoshsaha233@gmail.com>
2026-02-23 18:53:23 +0530

    feat: set top = -1

--- STACK/arrayStack.c
+++ STACK/arrayStack.c
@@ -3,53 +3,57 @@
 
 #define MAX_SIZE 10
 
-void display(int top, int *stack) {
-    for(int i = top - 1; i >= 0; i--) {
+void display(int top, int *stack, int n) {
+    if (top == -1) {
+        printf("Stack is empty\n");
+        return;
+    }
+    printf("Stack (bottom to top): ");
+    for (int i = 0; i <= top; i++) {
         printf("%d ", stack[i]);
     }
     printf("\n");
 }
 
-void push(int max, int *top, int *stack) {
-    if(*top >= max) {
+void push(int max, int *top, int *stack, int n) {
+    if (*top >= max - 1) {
         printf("Stack is full\n");
         return;
     }
     int x;
     printf("Enter the element to push: ");
     scanf("%d", &x);
-    stack[*top] = x;
     (*top)++;
-    display(*top, stack); 
+    stack[*top] = x;
+    display(*top, stack, n);
 }
 
-void pop(int *top, int *stack) {
-    if(*top <= 0) {
+void pop(int *top, int *stack, int n) {
+    if (*top == -1) {
         printf("Stack is empty\n");
         return;
     }
     (*top)--;
-    stack[*top] = 0;
-    display(*top, stack); 
+    display(*top, stack, n);
 }
 
 int main() {
     int n;
     printf("Enter the size of the stack: ");
     scanf("%d", &n);
-    if(n > MAX_SIZE) {
+    if (n > MAX_SIZE) {
         printf("Exceed max Stack size.");
         return 1;
     }
 
     int *stack = (int *)malloc(n * sizeof(int));
-    if(stack == NULL) {
+    if (stack == NULL) {
         printf("Memory Allocation Error.");
         return 1;
     }
-    
-    int top = 0;
-    while(1) {
+
+    int top = -1; // Start the stack from -1
+    while (1) {
         printf("1: Push\n");
         printf("2: Pop\n");
         printf("3: Exit\n");
@@ -58,12 +62,12 @@ int main() {
         printf("Enter your choice: ");
         scanf("%d", &x);
 
-        switch(x) {
+        switch (x) {
             case 1:
-                push(n, &top, stack); // Use 'n' instead of MAX_SIZE for user-specified size
+                push(n, &top, stack, n); // Last element is on top, use 0-based index
                 break;
-            case 2: 
-                pop(&top, stack);
+            case 2:
+                pop(&top, stack, n);
                 break;
             case 3:
                 free(stack);
@@ -74,6 +78,6 @@ int main() {
                 return 1;
         }
     }
-    
+
     return 0;
 }
