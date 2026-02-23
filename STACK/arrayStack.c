9cf205aeceee82d5540efdd91c4adb410a3efa75
Ash310u <ashutoshsaha233@gmail.com>
2026-02-23 16:11:19 +0530

    update stack code

--- STACK/arrayStack.c
+++ STACK/arrayStack.c
@@ -3,36 +3,67 @@
 
 #define MAX_SIZE 10
 
-int *initStack(int n);
-void push(int max, int *top, int *stack);
-void pop(int max, int *top, int *stack);
-void display(int top, int *stack);
+void display(int top, int *stack) {
+    for(int i = top - 1; i >= 0; i--) {
+        printf("%d ", stack[i]);
+    }
+    printf("\n");
+}
+
+void push(int max, int *top, int *stack) {
+    if(*top >= max) {
+        printf("Stack is full\n");
+        return;
+    }
+    int x;
+    printf("Enter the element to push: ");
+    scanf("%d", &x);
+    stack[*top] = x;
+    (*top)++;
+    display(*top, stack); 
+}
+
+void pop(int *top, int *stack) {
+    if(*top <= 0) {
+        printf("Stack is empty\n");
+        return;
+    }
+    (*top)--;
+    stack[*top] = 0;
+    display(*top, stack); 
+}
 
 int main() {
     int n;
     printf("Enter the size of the stack: ");
     scanf("%d", &n);
     if(n > MAX_SIZE) {
-        printf("Exceeded max Stack size.");
+        printf("Exceed max Stack size.");
+        return 1;
+    }
+
+    int *stack = (int *)malloc(n * sizeof(int));
+    if(stack == NULL) {
+        printf("Memory Allocation Error.");
         return 1;
     }
     
-    int *stack = initStack(n);
-    
     int top = 0;
-    while (1) {
-        printf("1. Push\n");
-        printf("2. Pop\n");
-        printf("3. Exit\n");
+    while(1) {
+        printf("1: Push\n");
+        printf("2: Pop\n");
+        printf("3: Exit\n");
+
         int x;
         printf("Enter your choice: ");
         scanf("%d", &x);
-        switch (x) {
+
+        switch(x) {
             case 1:
-                push(n, &top, stack);
+                push(n, &top, stack); // Use 'n' instead of MAX_SIZE for user-specified size
                 break;
-            case 2:
-                pop(n, &top, stack);
+            case 2: 
+                pop(&top, stack);
                 break;
             case 3:
                 free(stack);
@@ -40,49 +71,9 @@ int main() {
             default:
                 free(stack);
                 printf("Invalid choice\n");
-                break;
+                return 1;
         }
     }
     
     return 0;
-}
-
-int *initStack(int n) {
-    int *stack = (int *)malloc(n * sizeof(int));
-    if (stack == NULL) {
-        printf("Memory allocation failed\n");
-        return NULL;
-    }
-    return stack;
-}
-
-void push(int max, int *top, int *stack) {
-    if(*top < max) {
-        int x;
-        printf("Enter the element to push: ");
-        scanf("%d", &x);
-        stack[*top] = x;
-        (*top)++;
-        display(*top, stack);
-    } else {
-        printf("Stack is full\n");
-    }
-}
-
-void pop(int max, int *top, int *stack) {
-    if(*top > 0) {
-        stack[*top] = 0;
-        (*top)--;
-        display(*top, stack);
-    } else {
-        printf("Stack is empty\n");
-    }
-}
-
-void display(int top, int *stack) {
-    for(int i = top - 1; i >= 0; i--) {
-        printf("    %d    \n", stack[i]);
-        printf("__________\n");
-    }
-    printf("\n");
 }
